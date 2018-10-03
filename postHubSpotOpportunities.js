46129ca0ee6ec452b3dfd559aa56124cf93a9b1e
Admin User <adminuser@elements-mac114.local>
2018-10-03 14:12:49 -0600

    added slow loop

--- postHubSpotOpportunities.js
+++ postHubSpotOpportunities.js
@@ -5,7 +5,6 @@ const prompt = require('prompt');
 
 prompt.start();
 prompt.get(['Iterations (< 500)'], function (err, result) {
-  //const elementKey = "MhRU4yMJ0GvphVdugjvEkJKXmxgnv90XwW2hFeJqGrU="
   const iterations = `${result['Iterations (< 500)']}`;
   const authHeader = "User cD8xoBAixe+KZoQQqdSUWAtmi2bugDD1fwuQ3BI7V7I=, Organization afa1203433d54b98eb91931b75abc83e, Element P6EBzzR+YsnPt2hDmyoELy3P4LyHy75Twf6bkefd5G4=";
   const apiUrl =  "https://staging.cloud-elements.com/elements/api-v2/opportunities";
@@ -16,7 +15,6 @@ prompt.get(['Iterations (< 500)'], function (err, result) {
   let failedCounter = 0;
 
   const postOpportunities = function (runCounter) {
-  //  i++
     const options =  {
       'method': 'POST',
       'headers': {
@@ -36,25 +34,13 @@ prompt.get(['Iterations (< 500)'], function (err, result) {
     .then(function (response) {
       succesCounter++;
       console.log(`${succesCounter} successful POSTs to /opportunities`);
-      // if(i <= iterations){
-      //   setTimeout(function(){
-      //     postOpportunities();
-      //   }, 5000);
-      // }
     })
     .catch(function (err) {
       failedCounter++
       console.log(`ERROR: ${err}`);
       console.log(`${failedCounter} failed`);
-      // if(i <= iterations){
-      //   setTimeout(function(){
-      //     postOpportunities();
-      //   }, 5000);
-      // }
     });
   }
-
-
 
   if(iterations < 500){
       function slowLoop( count, interval, callback ) {
@@ -62,19 +48,15 @@ prompt.get(['Iterations (< 500)'], function (err, result) {
         next();
         function next() {
           if( callback( i ) !== false ) {
-                if( ++i < count ) {
+              if( ++i < count ) {
                   setTimeout( next, interval );
-                }
+              }
           }
         }
       }
-
-      slowLoop( iterations, 1000, function( i ) {
+      slowLoop(iterations, 1000, function( i ) {
           postOpportunities()
       });
-    // for (var i = 0; i <= iterations; ++i) {
-    //   postOpportunities();
-    // }
   } else {
     console.log("ERROR: Please enter an interation number less then 500")
   }
